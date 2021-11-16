b9b018c82e6ef35473009cef76b4d963c80615a6
snsepton <sonja.septon@utah.edu>
2021-11-16 14:58:45 -0700

    ElementbyID

---
+++ index.js
@@ -0,0 +1,3 @@
+var text = document.querySelector("#header").textContent
+document.querySelector("#paragraph").textContent = "Here is my version of the text through JS"
+document.querySelector("#header").textContent = "My text that I added through JS"
