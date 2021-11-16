a50c1352b81a6d8ad04f9764dcef9a0c8aaf921a
snsepton <sonja.septon@utah.edu>
2021-11-16 15:26:39 -0700

    GetElementbyID

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-var text = document.querySelector("#header").textContent
-document.querySelector("#paragraph").textContent = "Here is my version of the text through JS"
-document.querySelector("#header").textContent = "My text that I added through JS"
+var text = document.getElementById("header");
+text.textContent = "Here is my version of the text through JS"
